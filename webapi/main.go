445d1461eaf7a8617380305107a1a852cc8cf4bf
Shadow032020 <asma200318@gmail.com>
2024-12-12 14:57:16 +0100

    change go file

--- webapi/main.go
+++ webapi/main.go
@@ -19,7 +19,7 @@ func main() {
 
 func whoAmI(response http.ResponseWriter, r *http.Request) {
 	who := []whoami{
-		whoami{Name: "Efrei Paris",
+		whoami{Name: "Team Abidi He Wonyu Yu",
 			Title: "DevOps and Continous Deployment",
 			State: "FR",
 		},
